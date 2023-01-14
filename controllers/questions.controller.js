b5773c0cf854ad4c5afe765aeeb2d0328fb6ee97
CharlieEngler3 <charlieengler3@gmail.com>
2023-01-14 12:13:47 -0500

    Added Question Editor

--- controllers/questions.controller.js
+++ controllers/questions.controller.js
@@ -2,7 +2,7 @@ const db = require('../models');
 const Question = db.question;
 const Answer = db.answer;
 
-exports.getQuestions = async (req, res) => {
+exports.getQuestionsByTag = async (req, res) => {
     // TODO: Implement me
 
     const tags = req.body.tags;
@@ -16,6 +16,14 @@ exports.getQuestions = async (req, res) => {
     }
 
     res.send(questions);
+};
+
+exports.getQuestionByQuestion = (req, res) => {
+    const question = req.params.question;
+
+    Question.findOne({ question: { $regex: question, $options: "i" } }).then(fetchedQuestion => {
+        res.send(fetchedQuestion);
+    });
 };
 
 exports.addQuestion = async (req, res) => {
@@ -49,6 +57,96 @@ exports.addQuestion = async (req, res) => {
 
     newQuestion.save().then(() => {
         res.status(201).send("Added Question and Answers");
+    }).catch(error => {
+        res.status(400).send(error);
+    });
+};
+
+exports.updateQuestion = async (req, res) => {
+    const questionID = req.body.questionID;
+    const question = req.body.question;
+    const questionTags = req.body.questionTags;
+    const answerSorting = req.body.answerSorting;
+    const questionType = req.body.questionType;
+
+    // TODO: Check which answers were added by cross referencing the given answerIDs
+    // TODO: Check for changes in the answer tags
+    const answerIDs = req.body.answerIDs;
+    const existingAnswers = [];
+    const answers = req.body.answers;
+    const answerTags = req.body.answerTags;
+
+    for(let i = 0; i < answerIDs.length; i++){
+        const existingAnswer = await Answer.findOne({ _id: answerIDs[i] });
+    
+        existingAnswers.push(existingAnswer.answer);
+    }
+
+    for(let i = 0; i < answers.length; i++){
+        if(!existingAnswers.includes(answers[i])){
+            const newAnswerTags = answerTags[i] != null ? answerTags[i] : [];
+
+            const newAnswer = new Answer({
+                answer: answers[i],
+                tags: newAnswerTags
+            });
+
+            const newAnswerData = await newAnswer.save();
+            answerIDs.push(newAnswerData._id.toString());
+        }
+    }
+
+    for(let i = 0; i < answerIDs.length; i++){
+        const answer = await Answer.findOne({ _id: answerIDs[i] });
+        const newAnswerTags = answerTags[i] != null ? answerTags[i] : [];
+        const parsedAnswerTags = [];
+
+        newAnswerTags.map(answerTag => {
+            if(answerTag.length > 0){
+                parsedAnswerTags.push(answerTag);
+            }
+        })
+
+        answer.answer = answers[i];
+        answer.tags = parsedAnswerTags;
+        answer.save();
+    }
+
+    const parsedQuestionTags = [];
+    questionTags.map(questionTag => {
+        if(questionTag.length > 0){
+            parsedQuestionTags.push(questionTag);
+        }
+    });
+
+    const existingQuestion = await Question.findOne({ _id: questionID });
+    existingQuestion.question = question;
+    existingQuestion.answerIDs = answerIDs;
+    existingQuestion.tags = parsedQuestionTags;
+    existingQuestion.answerSorting = answerSorting;
+    existingQuestion.type = questionType;
+    existingQuestion.save().then(() => {
+        res.status(200).send("Question Updated Successfully");
+    }).catch(error => {
+        res.status(400).send(error);
+    });
+};
+
+exports.deleteQuestionAndAnswers = (req, res) => {
+    const questionID = req.params.id;
+
+    Question.findOne({ _id: questionID }).then(async question => {
+        const answerIDs = question.answerIDs;
+
+        for(let i = 0; i < answerIDs.length; i++){
+            await Answer.deleteOne({ _id: answerIDs[i] });
+        }
+
+        Question.deleteOne({ _id: questionID }).then(() => {
+            res.status(200).send("Question Deleted Successfully");
+        }).catch(error => {
+            res.status(400).send(error);
+        });
     }).catch(error => {
         res.status(400).send(error);
     });
--- routers/questions.router.js
+++ routers/questions.router.js
@@ -3,7 +3,11 @@ const questionController = require('../controllers/questions.controller.js');
 
 const router = express.Router();
 
-router.post('', questionController.getQuestions);
+router.post('', questionController.getQuestionsByTag);
 router.post('/add', questionController.addQuestion);
+router.post('/update', questionController.updateQuestion);
+
+router.get('/:question', questionController.getQuestionByQuestion);
+router.get('/delete/:id', questionController.deleteQuestionAndAnswers);
 
 module.exports = router;
